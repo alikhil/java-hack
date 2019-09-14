85b6583a690ed955f5260f8b1d64dd290dcab4b0
Khanda Kevin <kevin.khanda@gmail.com>
2019-09-14 14:55:12 +0300

    Add basic project initialization

---
+++ src/main/java/com/hardcoders/havajack/HavaJackApplication.java
@@ -0,0 +1,13 @@
+package com.hardcoders.havajack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavaJackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HavaJackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardcoders/havajack/HavaJackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hardcoders.havajack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HavaJackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
