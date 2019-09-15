ec25d1afe4d24a145b9041276de8f4d4f1e6fb8e
Alik Khilazhev <alikhil@mail.ru>
2019-09-15 14:03:31 +0300

    Refactor

--- src/main/java/com/hardcoders/havajack/controller/ClientController.kt
+++ src/main/java/com/hardcoders/havajack/controller/ClientController.kt
@@ -1,7 +1,8 @@
 package com.hardcoders.havajack.controller
 
+import com.hardcoders.havajack.dto.TokenDto
 import com.hardcoders.havajack.model.Client
-import com.hardcoders.havajack.model.registration.SmzAccountDto
+import com.hardcoders.havajack.service.ClientService
 import com.hardcoders.havajack.service.SmzService
 import org.springframework.web.bind.annotation.*
 
@@ -9,18 +10,18 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/client")
 class ClientController(val smzService: SmzService, val clientService: ClientService)
  {
-    @PostMapping("/new")
-    fun new(@RequestBody account: SmzAccountDto): String {
-        return clientService.createAccount(account)
-    }
+//    @PostMapping("/new")
+//    fun new(@RequestBody token: TokenDto): String {
+//        return clientService.createClient(token.token)
+//    }
 
     @GetMapping("/info")
     fun getInfo(@RequestParam token: String): Client? {
-        return clientService.getClient(token);
+        return clientService.getClient(token)
     }
 
     @PostMapping("/pay")
-    fun payTaxes(@RequestParam token: String): String {
-        return clientService.payTaxes(token);
+    fun payTaxes(@RequestParam token: TokenDto): String {
+        return clientService.payTaxes(token.token)
     }
 }
